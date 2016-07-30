d7f4f7cf9c396ca56a991daf7a9c0614d673c66f
caden <cadenkline9@gmail.com>
2016-07-30 18:53:47 -0400

    Initial commit

---
+++ src/main/java/com/k_linedev/game/logic.java
@@ -0,0 +1,14 @@
+package com.k_linedev.game;
+
+import java.util.ArrayList;
+
+/**
+ * Created by caden on 7/30/16.
+ */
+public class logic {
+    static ArrayList<String> usedWords = new ArrayList<String>();
+    public static void addWord(){
+        
+    }
+
+}
